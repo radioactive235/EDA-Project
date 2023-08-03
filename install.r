859c195594775d1daf2d31b3ca2e9b8f4ca03408
Anji Ni <102392938+radioactive235@users.noreply.github.com>
2023-08-03 00:05:14 -0400

    Create install.r

---
+++ install.r
@@ -0,0 +1,7 @@
+install.packages("tidyverse")
+install.packages(cowplot)
+install.packages(scales)
+install.packages(tigris)
+install.packages(leaflet)
+install.packages(htmlwidgets)
+install.packages(htmltools)
